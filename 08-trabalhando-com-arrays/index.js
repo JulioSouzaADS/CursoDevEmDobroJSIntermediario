37c5b3911e98cd85c857b89b55bed4c4605f7549
Julio Souza <visoboti@gmail.com>
2022-08-15 10:18:45 -0300

    15-08

--- 08-trabalhando-com-arrays/index.js
+++ 08-trabalhando-com-arrays/index.js
@@ -10,9 +10,27 @@
 
 // segunda forma de fazer o mesmo processo 
 
+
 let frutas = [ 'banana' ,  'maça' , 'laranja', 'pêra'];
 let frutasTamanho = frutas.length;//utlizada para identificar a quantidade de itens deste array
 
-frutas.forEach( function (item,){
+let posicaoLaranja = frutas.indexOf ('laranja')
+console.log(posicaoLaranja)
+
+frutas.push ('Manga') // metodo para adicionar um item na ultima posicao do array
+console.log( frutas)
+
+frutas.pop () // metodo para remover o ultimo elemento do array
+console.log( frutas)
+
+frutas.shift ('banana') // metodo que remove o primeiro elemento de índice zero
+console.log( frutas)
+
+frutas.unshift ('morango') // metodo que adiciona um elemento no primeiro elemento de índice zero
+console.log( frutas)
+
+frutas.forEach( function (item){
     console.log (item)
+
 })
+
